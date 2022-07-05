c4011e9b1a8147f6a2cb263ca706a14066e7dc3e
Jhonatan <jonathandiazp@gmail.com>
2022-07-04 21:21:59 -0400

    building question function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,52 @@ var userQuestions = [
     {
         question: "Commonly used data types Do not include:",
         answers: {
-                   0: "1. strings",
-                   1: "2. booleans",
-                   2: "3. alerts",
-                   3: "4. numbers"
+                   0: "strings",
+                   1: "booleans",
+                   2: "alerts",
+                   3: "numbers"
                  },
-        correctAnswer: "2"
+        correctAnswer: "alerts"
     },
     {
         question: "The condition in an if / else statement enclosed with __________.",
         answers: {
-                   0: "1. quotes",
-                   1: "2. curly brackets",
-                   2: "3. parenthesis",
-                   3: "4. square brackets"
+                   0: "quotes",
+                   1: "curly brackets",
+                   2: "parenthesis",
+                   3: "square brackets"
                  },
-        correctAnswer: "2"
+        correctAnswer: "parenthesis"
     },
     {
         question: "Arrays in JavaScript can be be used to store ________.",
         answers: {
-                   0: "1. numbers and strings",
-                   1: "2. other arrays",
-                   2: "3. booleans",
-                   3: "4. all the above"
+                   0: "numbers and strings",
+                   1: "other arrays",
+                   2: "booleans",
+                   3: "all the above"
                  },
-        correctAnswer: "2"
+        correctAnswer: "all the above"
     },
     {
         question: "String values must be enclosed within ______ when being assigned to variables.",
         answers: {
-                   0: "1. commas",
-                   1: "2. curly brackets",
-                   2: "3. quotes",
-                   3: "4. paranthesis"
+                   0: "commas",
+                   1: "curly brackets",
+                   2: "quotes",
+                   3: "paranthesis"
                  },
-        correctAnswer: "2"
+        correctAnswer: "quotes"
     },
     {
         question: "A very useful tool used during developement and debugging for printing content to the debugger is:",
         answers: {
-                   0: "1. JavaScript",
-                   1: "2. terminal/bash",
-                   2: "3. for loops",
-                   3: "4. console.log"
+                   0: "JavaScript",
+                   1: "terminal/bash",
+                   2: "for loops",
+                   3: "console.log"
                  },
-        correctAnswer: "2"
+        correctAnswer: "console.log"
     }  
 ];
 
@@ -56,95 +56,122 @@ var userWrongAnswer;
 var questionIndex = 0;
 var userCorrectAnswer = 0 
 var timeLeft = 75;
-
+var question;
+var answer1,answer2,answer3,answer4;
+var previousScores;
+var questionAnswerCorrect;
+var selectionArray = [];
+var buttonArray = [];
+var questionAnswer;
+totalQuestions = userQuestions.length;
 var timer = document.querySelector("#timer");
-var scoreLink = document.querySelector("#scorelink");
+var scoreLink = document.querySelector("scorelink");
 var principalContent = document.querySelector("#principal-content");
-var questionSwitch = document.getElementById("#question-change");
+var questionSwitch = document.getElementById("question-change");
 var quizQuestion = document.querySelector("#quizquestion");
 var userSelection = document.querySelector("#userselection");
 var userScore = document.querySelector("#userscore");
 var userHighScore = document.querySelector("#userhighscores");
 var score = document.getElementById("#userhighscores");
+var startButton = document.getElementById("startquiz");
 
 
-//Counter function
-var startCounter = function() {
-var Counter = setInterval(function () {
-    timeLeft = timeLeft - userWrongCounter;
-    if (timeLeft >= 0) {
-      document.getElementById("time").innerHTML = timeLeft;
+for(var i = 0; i < 4; i++){
+    var buttonDiv = document.createElement("div");
+    var selectionButtons = document.createElement("button");
+    selectionButtons.setAttribute("data-index", i);
+    selectionButtons.setAttribute("class","btnn");
+    selectionArray.push(buttonDiv);
+    buttonArray.push(selectionButtons);
+}
+
+var rightWrong = document.createElement("p");
+rightWrong.setAttribute("class", "right-wrong");
+userSelection.appendChild(rightWrong);
+
+
+function startChallenge(){
+    startTimer();
+    buildQuestion();
+}
+
+function startTimer(){
+    
+    var timeInterval = setInterval(function(){
+
         timeLeft--;
 
-
+        timer.textContent = "Time : "+timeLeft;
+        
+        if(timeLeft <= 0 || (questionIndex > totalQuestions-1)){
+            
+            userSelection.style.display = "none";
+            quizQuestion.style.display = "none";
+            viewResult();
+            clearInterval(timeInterval);
+            timer.textContent = "";
         }
-    else {
-        clearInterval(timeInterval);
-        quizResult();
-    }
-    }, 1000);
-};
-//End counter Function
 
+    },1000);
+}
 var buildQuestion = function(){
 
-questionList.removeChild(listPosted);
-startButton.removeChild(deleteStartButton);
-listEl.setAttribute('style', 'padding:0; margin:0;list-style: none;text-align:left;');
-listEl.setAttribute('id','list');
-questionSwitch.setAttribute('style', 'font-size:25px; text-align:left;');
+    questionSwitch.style.display = "none";
+    principalContent.style.display= "none";
+    quizQuestion.style.display = "none";
 
-    showQuestion(); 
-};
-
-var showQuestion = function(){
-
-    questionSwitch.innerHTML = userQuestions[a].question;
-    questionSwitch.setAttribute('style', 'font-size:25px; margin-top:  120px; text-align:left;');
-    activeQuestion = questionSwitch.innerHTML;
-    showAnswers();
-};  
-var showAnswers = function(){
-
-    for (var i = 0; i < userQuestions.length-1; i++){
-
-    var userSelection = document.createElement('li');
-    var answerButton = document.createElement("button")
-    var t = userQuestions[a].answers[i];
-   answerButton.innerHTML = t;
-   answerButton.setAttribute('id', [i]);
-   answerButton.setAttribute('class','option-button');
-    userSelection.appendChild(answerButton);
-    listEl.appendChild(userSelection);
+    if(questionIndex > totalQuestions -1){
+        return;
     }
-    questionList.appendChild(listEl);
-    buttonPressed();
-};
-var buttonPressed = function(){
+    else{
+        questionAnswerCorrect = userQuestions[questionIndex].correctAnswer;
 
-    var buttonEl0 = document.getElementById("0");
-    var buttonEl1 = document.getElementById("1");
-    var buttonEl2 = document.getElementById("2");
-    var buttonEl3 = document.getElementById("3");
+        questionSwitch.innerHTML = userQuestions[questionIndex].question;
+        questionSwitch.setAttribute("class","textleft;");
+        questionSwitch.style.display="block";
 
-    buttonEl0.addEventListener("click", function(){
-    pressedButton = 0;
-        });
-    buttonEl1.addEventListener("click", function(){
-    pressedButton = 1;
-        });
-    buttonEl2.addEventListener("click", function(){
-    pressedButton = 2;
-        });    
-    buttonEl3.addEventListener("click", function(){
-    pressedButton = 3;
-        });
-};
-    
+        for (var a = 0; a < 4; a++){
+        var index = buttonArray[a].getAttribute("data-index");
+        buttonArray[a].textContent = (+index+1) +". "+userQuestions[questionIndex].answers[index];
+        selectionArray[a].appendChild(buttonArray[a]);
+        quizQuestion.appendChild(selectionArray[a]);
 
-var checkAnswer = function(){
+        }
 
-};
+    }
+    quizQuestion.setAttribute("style","text-align:left");
+    quizQuestion.style.display = "block";
+}
 
-// document.getElementById("summit").addEventListener("click", startCounter);
-// document.getElementById("summit").addEventListener("click", buildQuestion);
+quizQuestion.addEventListener("click",function(event){
+
+        var segment = event.target;
+        var selectedAnswer = segment.textContent;
+        var options = selectedAnswer.substring(3, selectedAnswer.length);
+
+        if (options === questionAnswerCorrect){
+            userCorrectAnswer++;
+            userSelection.style.display = "block";
+            rightWrong.textContent = "correct!";
+
+            setTimeout(function(){
+                rightWrong.textContent="";
+
+            },500);
+        }
+        else {
+            timeLeft -= 10;
+            rightWrong.textContent = "Wrong!";
+
+            setTimeout(function(){
+                rightWrong.textContent="";
+            },500);
+        }
+        questionIndex++;
+        buildQuestion();
+
+});
+
+
+
+startButton.addEventListener("click",startChallenge);
